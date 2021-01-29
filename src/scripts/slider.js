b7f98f0c468f4cebb65cff01baa374e85599fec5
Golubev Evgeny <limp061289@gmail.com>
2021-01-29 20:34:10 +0300

    change of banks cards

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,54 +1,55 @@
-const slyder = document.querySelector('.banks__carousel');
-const slyderItem = document.querySelector('.banks__carousel-item')
-const style = getComputedStyle(slyderItem);
+(function sliderScroll() {
+    const slider = document.querySelector('.banks__carousel');
+    const sliderItem = document.querySelector('.banks__carousel-item')
+    const style = getComputedStyle(sliderItem);
 
-//dimensions
-const itemWidth = parseInt(style.width.match(/\d+/));
-const itemMarginRight = parseInt(style.marginRight.match(/\d+/));
-const itemMarginLeft = parseInt(style.marginLeft.match(/\d+/));
-const scrollStep = itemWidth +itemMarginLeft + itemMarginRight;
+    //dimensions
+    const itemWidth = parseInt(style.width.match(/\d+/));
+    const itemMarginRight = parseInt(style.marginRight.match(/\d+/));
+    const scrollStep = itemWidth + itemMarginRight;
 
-//amount of elements
-const countItem = slyder.children.length;
-const maxItemsOnScreen = Math.floor(parseInt(document.body.clientWidth) / scrollStep);
+    //amount of elements
+    const countItem = slider.children.length;
+    const maxItemsOnScreen = Math.floor(parseInt(document.body.clientWidth) / scrollStep);
 
-//scroll
-const maxScrollCount = (countItem - maxItemsOnScreen) * scrollStep;
-let currentPosition = style.transform.match(/\d+/);
-
+    //scroll
+    const maxScrollCount = (countItem - maxItemsOnScreen) * scrollStep;
+    let currentPosition = style.transform.match(/\d+/);
 
 
-function scrollSlyder(event) {
-    const wheelX = event.deltaY;
-    if (currentPosition === null) {
-        currentPosition = 0
+
+    function scrollSlyder(event) {
+        const wheelX = event.deltaY;
+        if (currentPosition === null) {
+            currentPosition = 0
+        }
+        console.log(currentPosition)
+
+        if (wheelX > 0 && currentPosition > -maxScrollCount) {
+            currentPosition -= scrollStep
+            slider.style.transform = `translateX(${currentPosition}px)`;
+        }
+
+        if (wheelX < 0 && currentPosition < 0) {
+            currentPosition += scrollStep
+            slider.style.transform = `translateX(${currentPosition}px)`;
+        }
     }
-    console.log(currentPosition)
-
-    if (wheelX > 0 && currentPosition > -maxScrollCount ) {
-        currentPosition -= scrollStep
-        slyder.style.transform = `translateX(${currentPosition}px)`;
-    }
-
-    if (wheelX < 0 && currentPosition < 0) {
-        currentPosition += scrollStep
-        slyder.style.transform = `translateX(${currentPosition}px)`;
-    }
-}
 
 
-slyder.addEventListener("wheel", (event) => {
-    scrollSlyder(event);
-});
+    slider.addEventListener("wheel", (event) => {
+        scrollSlyder(event);
+    });
 
-slyder.addEventListener("mouseover", (event) => {
-    if (event.currentTarget === slyder) {
-        document.body.style.overflow = "hidden"
-    }
-})
+    slider.addEventListener("mouseover", (event) => {
+        if (event.currentTarget === slider) {
+            document.body.style.overflow = "hidden"
+        }
+    })
 
-slyder.addEventListener("mouseout", (event) => {
-    if (event.currentTarget === slyder) {
-        document.body.style.overflow = "visible"
-    }
-})
+    slider.addEventListener("mouseout", (event) => {
+        if (event.currentTarget === slider) {
+            document.body.style.overflow = "visible"
+        }
+    })
+}())
